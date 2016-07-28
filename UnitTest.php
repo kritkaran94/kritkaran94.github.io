4cd55c73d9667c8ccf7031a026d58127de1005c0
Krit Karan <kritkaran94@users.noreply.github.com>
2016-07-28 14:53:08 +0530

    [ip] Travis testing

---
+++ UnitTest.php
@@ -0,0 +1,537 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+
+	<title>Krit Karan Singh</title>
+
+
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/freelancer.min.css" rel="stylesheet">
+	<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+</head>
+
+<body id="page-top" class="index">
+
+	<!-- Navigation -->
+	<nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+		<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header page-scroll">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+				</button>
+				<a class="navbar-brand" href="#page-top">Krit Karan Singh</a>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="hidden">
+						<a href="#page-top"></a>
+					</li>
+					<li class="page-scroll">
+						<a href="#portfolio">Portfolio</a>
+					</li>
+					<li class="page-scroll">
+						<a href="#about">Resume</a>
+					</li>
+					<li class="page-scroll">
+						<a href="#contact">Contact</a>
+					</li>
+				</ul>
+			</div>
+			<!-- /.navbar-collapse -->
+		</div>
+		<!-- /.container-fluid -->
+	</nav>
+
+	<!-- Header -->
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<img class="img-responsive" src="img/krit_web.png">
+					<div class="intro-text">
+						<span class="name">Krit Karan</span>
+						<hr class="star-light">
+						<span class="skills">Software Developer - Open Source Enthusiast - Foodholic</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<!-- Portfolio Grid Section -->
+	<section id="portfolio">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 text-center">
+					<h2>Portfolio</h2>
+					<hr class="star-primary">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-4 portfolio-item">
+					<a href="#portfolioModal1" class="portfolio-link" data-toggle="modal">
+						<div class="caption">
+							<div class="caption-content">
+								<i class="fa fa-search-plus fa-3x"></i>
+							</div>
+						</div>
+						<img src="img/television-1.png" class="img-responsive" alt="">
+					</a>
+				</div>
+				<div class="col-sm-4 portfolio-item">
+					<a href="#portfolioModal2" class="portfolio-link" data-toggle="modal">
+						<div class="caption">
+							<div class="caption-content">
+								<i class="fa fa-search-plus fa-3x"></i>
+							</div>
+						</div>
+						<img src="img/router.png" class="img-responsive" alt="">
+					</a>
+				</div>
+				<div class="col-sm-4 portfolio-item">
+					<a href="#portfolioModal3" class="portfolio-link" data-toggle="modal">
+						<div class="caption">
+							<div class="caption-content">
+								<i class="fa fa-search-plus fa-3x"></i>
+							</div>
+						</div>
+						<img src="img/ram-memory.png" class="img-responsive" alt="">
+					</a>
+				</div>
+				<div class="col-sm-4 portfolio-item">
+					<a href="#portfolioModal4" class="portfolio-link" data-toggle="modal">
+						<div class="caption">
+							<div class="caption-content">
+								<i class="fa fa-search-plus fa-3x"></i>
+							</div>
+						</div>
+						<img src="img/computer.png" class="img-responsive" alt="">
+					</a>
+				</div>
+				<div class="col-sm-4 portfolio-item">
+					<a href="#portfolioModal5" class="portfolio-link" data-toggle="modal">
+						<div class="caption">
+							<div class="caption-content">
+								<i class="fa fa-search-plus fa-3x"></i>
+							</div>
+						</div>
+						<img src="img/macbook.png" class="img-responsive" alt="">
+					</a>
+				</div>
+				<div class="col-sm-4 portfolio-item">
+					<a href="#portfolioModal6" class="portfolio-link" data-toggle="modal">
+						<div class="caption">
+							<div class="caption-content">
+								<i class="fa fa-search-plus fa-3x"></i>
+							</div>
+						</div>
+						<img src="img/server.png" class="img-responsive" alt="">
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- About Section -->
+	<section class="success" id="about">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 text-center">
+					<h2>Resume</h2>
+					<hr class="star-light">
+					<object data="Web_Resume.pdf" type="application/pdf" width="70%" height="800px">
+  <p>It appears you don't have a PDF plugin for this browser.
+   No Issues... you can <a href="resume.pdf">click here to
+  download my resume and view locally on your device.</a></p>
+ </object>
+ </div>
+				   <!-- <iframe src="Web_Resume.pdf" style="width:700px; height:600px;"></iframe>-->
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Contact Section -->
+	<section id="contact">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 text-center">
+					<h2>Reach Me</h2>
+					<hr class="star-primary">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
+					<!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
+					<form name="sentMessage" id="contactForm" novalidate>
+						<div class="row control-group">
+							<div class="form-group col-xs-12 floating-label-form-group controls">
+								<label>Name</label>
+								<input type="text" class="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name.">
+								<p class="help-block text-danger"></p>
+							</div>
+						</div>
+						<div class="row control-group">
+							<div class="form-group col-xs-12 floating-label-form-group controls">
+								<label>Email Address</label>
+								<input type="email" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address.">
+								<p class="help-block text-danger"></p>
+							</div>
+						</div>
+						<div class="row control-group">
+							<div class="form-group col-xs-12 floating-label-form-group controls">
+								<label>Message</label>
+								<textarea rows="5" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
+								<p class="help-block text-danger"></p>
+							</div>
+						</div>
+						<br>
+						<div id="success"></div>
+						<div class="row">
+							<div class="form-group col-xs-12">
+								<button type="submit" class="btn btn-success btn-lg">Send</button>
+							</div>
+							<div>
+							<a href="https://www.facebook.com/kritkaran.singh">
+							<i class="fa fa-facebook-official" style="font-size:60px;" hspace="20;"></i>
+							</a>
+							<a href="https://github.com/kritkaran94">
+							<i class="fa fa-github-square" style="font-size:60px;" hspace="50;"></i>
+							</a>
+							<a href="https://plus.google.com/u/0/+KritKaranSinghBharadwaj">
+							<i class="fa fa-google-plus-square" style="font-size:60px;" hspace="20px;"></i>
+							</a>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Footer -->
+	<footer class="text-center">
+		<div class="footer-below">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						Copyright &copy; Krit Karan 2016
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+	<div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+		<a class="btn btn-primary" href="#page-top">
+			<i class="fa fa-chevron-up"></i>
+		</a>
+	</div>
+
+	<!-- Portfolio Modals -->
+	<div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-content">
+			<div class="close-modal" data-dismiss="modal">
+				<div class="lr">
+					<div class="rl">
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2">
+						<div class="modal-body">
+							<h2>Password Importer</h2>
+							<hr class="star-primary">
+							<img src="img/television-1.png" class="img-responsive img-centered" alt="">
+							<p> Developed a cross browser passwords importing tool which is now licensed under EPIC Privacy Browser.</p>
+							<ul class="list-inline item-details">
+								<li>Skills:
+									<strong>
+									C, C++, Python
+									</strong>
+								</li>
+								<li>Year:
+									<strong>
+									  2016
+									</strong>
+								</li>
+								<li>Role:
+									<strong>
+									 Software Engineer Intern (Mac, Windows)
+									</strong>
+								</li>
+								<li>Company:
+									<strong>
+									 Hidden Reflex
+									</strong>
+								</li>
+							</ul>
+							<button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-content">
+			<div class="close-modal" data-dismiss="modal">
+				<div class="lr">
+					<div class="rl">
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2">
+						<div class="modal-body">
+							<h2>Application Layer File Transfer Protocol</h2>
+							<hr class="star-primary">
+							<img src="img/router.png" class="img-responsive img-centered" alt="">
+							<p>Enabled both TCP and UDP based transfer of files as per the request of clients</p>
+							<ul class="list-inline item-details">
+								<li>Skills:
+									<strong>
+									C++, Socket Programming
+									</strong>
+								</li>
+								<li>Year:
+									<strong>
+									  2015
+									</strong>
+								</li>
+								<li>Role:
+									<strong>
+									 Networking Student
+									</strong>
+								</li>
+								<li>Company:
+									<strong>
+									 IIIT Sri City
+									</strong>
+								</li>
+							</ul>
+							<button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-content">
+			<div class="close-modal" data-dismiss="modal">
+				<div class="lr">
+					<div class="rl">
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2">
+						<div class="modal-body">
+							<h2>Hardware Acceleration Of Raspberry Pi</h2>
+							<hr class="star-primary">
+							<img src="img/ram-memory.png" class="img-responsive img-centered" alt="">
+							<p> Ensured Hardware Acceleration of Rpi through OpenGL ES 2.0 in VisPy using the pi3d module. </p>
+							<ul class="list-inline item-details">
+								<li>Skills:
+									<strong>
+									Python, OpenGL
+									</strong>
+								</li>
+								<li>Year:
+									<strong>
+									  2015
+									</strong>
+								</li>
+								<li>Role:
+									<strong>
+									 Open Source Developer
+									</strong>
+								</li>
+								<li>Company:
+									<strong>
+									 VisPy
+									</strong>
+								</li>
+							</ul>
+							<button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="portfolio-modal modal fade" id="portfolioModal4" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-content">
+			<div class="close-modal" data-dismiss="modal">
+				<div class="lr">
+					<div class="rl">
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2">
+						<div class="modal-body">
+							<h2>Facebook Scrapping System</h2>
+							<hr class="star-primary">
+							<img src="img/computer.png" class="img-responsive img-centered" alt="">
+							<p> Developed a system which scrapped all comments from any facebook page in a hierarchical manner.</p>
+							<ul class="list-inline item-details">
+								<li>Skills:
+									<strong>
+									Python, Graph API, urllib2, Beautiful Soup
+									</strong>
+								</li>
+								<li>Year:
+									<strong>
+									  2016
+									</strong>
+								</li>
+								<li>Role:
+									<strong>
+									 Data Science Student
+									</strong>
+								</li>
+								<li>Company:
+									<strong>
+									 Self Driven
+									</strong>
+								</li>
+							</ul>
+							<button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="portfolio-modal modal fade" id="portfolioModal5" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-content">
+			<div class="close-modal" data-dismiss="modal">
+				<div class="lr">
+					<div class="rl">
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2">
+						<div class="modal-body">
+							<h2>Human Action Recognition System</h2>
+							<hr class="star-primary">
+							<img src="img/macbook.png" class="img-responsive img-centered" alt="">
+							<p> Developed a system which recognised various human actions based on the pipeline of DTF and Fischer Vector.</p>
+							<ul class="list-inline item-details">
+								<li>Skills:
+									<strong>
+									Matlab, Blender, Image Processing
+									</strong>
+								</li>
+								<li>Year:
+									<strong>
+									  2015
+									</strong>
+								</li>
+								<li>Role:
+									<strong>
+									 Image Processing Student
+									</strong>
+								</li>
+								<li>Company:
+									<strong>
+									 Self Driven
+									</strong>
+								</li>
+							</ul>
+							<button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="portfolio-modal modal fade" id="portfolioModal6" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-content">
+			<div class="close-modal" data-dismiss="modal">
+				<div class="lr">
+					<div class="rl">
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2">
+						<div class="modal-body">
+							<h2>Smart Robotic Assistant</h2>
+							<hr class="star-primary">
+							<img src="img/server.png" class="img-responsive img-centered" alt="">
+							<p>A smart human assistant which operates on voice commands. There are five published IEEE Papers on this work.</p>
+							<ul class="list-inline item-details">
+								<li>Skills:
+									<strong>
+									Arduino Uno, Speech Signal Processing
+									</strong>
+								</li>
+								<li>Year:
+									<strong>
+									  2014
+									</strong>
+								</li>
+								<li>Role:
+									<strong>
+									 Speech Signal Processing Student
+									</strong>
+								</li>
+								<li>Company:
+									<strong>
+									 IIIT Sri City
+									</strong>
+								</li>
+							</ul>
+							<button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- jQuery -->
+	<script src="vendor/jquery/jquery.min.js"></script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+	<!-- Plugin JavaScript -->
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+	<!-- Contact Form JavaScript -->
+	<script src="js/jqBootstrapValidation.js"></script>
+	<script src="js/contact_me.js"></script>
+
+	<!-- Theme JavaScript -->
+	<script src="js/freelancer.min.js"></script>
+
+</body>
+
+</html>
